d8866dd921ab912b462de71a5db46bf825ef7b2d
m-o-d-e-r <moder.mode.on@gmail.com>
2024-09-29 14:07:06 +0300

    configure api instance

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -6,3 +6,7 @@ output "web_instance_public_ip" {
 output "sftp_instance_public_ip" {
   value = aws_instance.sftp.public_ip
 }
+
+output "api_instance_public_ip" {
+  value = aws_instance.api.public_ip
+}
--- terraform/aws/scripts/sync_remote_sftp.sh
+++ terraform/aws/scripts/sync_remote_sftp.sh
@@ -5,9 +5,11 @@ if [[ -z "$REMOTE_SFTP_IP" ]]; then
   exit 1
 fi
 
-until nc $REMOTE_SFTP_IP 22 &>/dev/null; do
+until nc -z $REMOTE_SFTP_IP 22 &>/dev/null; do
   echo "Waiting for $REMOTE_SFTP_IP to be reachable..."
   sleep 5
 done
+
+echo "Starting synchronization..."
 
 rsync -urltv --delete -e "ssh -o StrictHostKeyChecking=no -i keys/id_ed25519 -q" ./artifacts/ sftpuser@$REMOTE_SFTP_IP:/var/sftp/data/
