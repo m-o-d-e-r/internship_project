5a0cf02cfff7f5242d14db9c97b020f54a38fc2a
m-o-d-e-r <moder.mode.on@gmail.com>
2024-09-30 17:54:40 +0300

    connect dbs with API

--- terraform/aws/api_instance.tf
+++ terraform/aws/api_instance.tf
@@ -1,15 +1,11 @@
 
 resource "aws_key_pair" "api_key_pair" {
-  provider = aws.eu-north-1
-
   key_name   = "api_instance_key"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAf6V4g7ahli0x4Z8D3cbC3ZeT5li7p63zldIU0Wcijj"
 }
 
 
 resource "aws_subnet" "api_subnet" {
-  provider = aws.eu-north-1
-
   vpc_id     = aws_vpc.schedule_vpc.id
   cidr_block = "192.168.200.0/24"
 
@@ -26,17 +22,16 @@ resource "aws_route_table_association" "api_public_route_association" {
 
 
 resource "aws_instance" "api" {
-  provider = aws.eu-north-1
-
-  ami           = "ami-0129bfde49ddb0ed6" # Amazon Linux
-  instance_type = "t3.micro"
+  ami           = "ami-0b45ae66668865cd6" # Amazon Linux
+  instance_type = "t2.micro"
 
   key_name = aws_key_pair.api_key_pair.key_name
 
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.api_subnet.id
   vpc_security_group_ids = [
-    aws_security_group.schedule_security_group.id
+    aws_security_group.schedule_security_group.id,
+    aws_security_group.dbs_security_group.id
   ]
 
   root_block_device {
@@ -53,9 +48,17 @@ resource "aws_instance" "api" {
     sftp_instance_ip      = aws_instance.sftp.private_ip,
     sftp_user_private_key = base64encode(file("./keys/id_ed25519"))
     download_api_tar      = base64encode(file("./scripts/download_api_tar.sh"))
+    update_hosts          = base64encode(file("./scripts/update_hosts.sh"))
+    db_address            = aws_db_instance.postgres.address
+    redis_address         = aws_elasticache_cluster.redis.cache_nodes[0].address
+#    mongo_address         = aws_docdb_cluster_instance.mongo_cluster_instance.endpoint
+    restore_db_script     = base64encode(file("./scripts/restore_db.sh"))
   })
 
   depends_on = [
-    aws_instance.sftp
+    aws_instance.sftp,
+    aws_db_instance.postgres,
+    aws_elasticache_cluster.redis,
+#    aws_docdb_cluster_instance.mongo_cluster_instance
   ]
 }
--- terraform/aws/dbs_services.tf
+++ terraform/aws/dbs_services.tf
@@ -1,8 +1,52 @@
 # TODO: configure network
 
+resource "aws_subnet" "dbs_subnet_1" {
+  vpc_id            = aws_vpc.schedule_vpc.id
+  cidr_block        = "192.168.10.0/24"
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "dbs-subnet-1"
+  }
+}
+
+resource "aws_subnet" "dbs_subnet_2" {
+  vpc_id            = aws_vpc.schedule_vpc.id
+  cidr_block        = "192.168.20.0/24"
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    Name = "dbs-subnet-2"
+  }
+}
+
+resource "aws_route_table_association" "dbs1_public_route_association" {
+  subnet_id      = aws_subnet.dbs_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "dbs2_public_route_association" {
+  subnet_id      = aws_subnet.dbs_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+
 # redis configuration
+resource "aws_elasticache_subnet_group" "schedule_redis_subnet_group" {
+  name = "schedule-redis-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.dbs_subnet_1.id,
+    aws_subnet.dbs_subnet_2.id
+  ]
+
+  tags = {
+    Name = "schedule-redis-subnet-group"
+  }
+}
+
 resource "aws_elasticache_cluster" "redis" {
-  provider = aws.eu-north-1
   cluster_id           = "schedule-redis-cluster"
   engine               = "redis"
   node_type            = "cache.t3.micro"
@@ -10,13 +54,31 @@ resource "aws_elasticache_cluster" "redis" {
   parameter_group_name = "default.redis4.0"
   engine_version       = "4.0.10"
   port                 = 6379
+
+  security_group_ids = [
+    aws_security_group.dbs_security_group.id,
+    aws_security_group.schedule_security_group.id
+  ]
+
+  subnet_group_name = aws_elasticache_subnet_group.schedule_redis_subnet_group.name
 }
 # redis configuration end
 
 
 # postgres configuration
+resource "aws_db_subnet_group" "schedule_db_subnet_group" {
+  name = "schedule-db-subnet-group"
+  subnet_ids = [
+    aws_subnet.dbs_subnet_1.id,
+    aws_subnet.dbs_subnet_2.id
+  ]
+
+  tags = {
+    Name = "schedule-db-subnet-group"
+  }
+}
+
 resource "aws_db_instance" "postgres" {
-  provider = aws.eu-north-1
   allocated_storage    = 10
   db_name              = "schedule" # get from env
   engine               = "postgres"
@@ -26,26 +88,50 @@ resource "aws_db_instance" "postgres" {
   password             = "password"        # get from env
   parameter_group_name = "default.postgres16"
   skip_final_snapshot  = true
+
+  vpc_security_group_ids = [
+    aws_security_group.dbs_security_group.id,
+    aws_security_group.schedule_security_group.id
+  ]
+
+  db_subnet_group_name = aws_db_subnet_group.schedule_db_subnet_group.name
 }
 # postgres configuration end
 
 
 # mongo configuration
-resource "aws_docdb_cluster" "default" {
-  provider = aws.eu-west-2
-  cluster_identifier      = "schedule-docdb"
-  engine                  = "docdb"
-  engine_version          = "5.0"
-  master_username         = "username"
-  master_password         = "password"
-  skip_final_snapshot     = true
-}
-
-resource "aws_docdb_cluster_instance" "cluster_instance" {
-  provider = aws.eu-west-2
-  count              = 1
-  identifier         = "schedule-docdb-instance-${count.index}"
-  cluster_identifier = aws_docdb_cluster.default.id
-  instance_class     = "db.t3.medium"
-}
+#resource "aws_db_subnet_group" "schedule_mongo_subnet_group" {
+#  name = "schedule-mongo-subnet-group"
+#  subnet_ids = [
+#    aws_subnet.dbs_subnet_1.id,
+#    aws_subnet.dbs_subnet_2.id
+#  ]
+#
+#  tags = {
+#    Name = "schedule-mongo-subnet-group"
+#  }
+#}
+#
+#resource "aws_docdb_cluster" "mongo_cluster" {
+#  cluster_identifier  = "schedule-docdb"
+#  engine              = "docdb"
+#  engine_version      = "4.0"
+#  master_username     = "username"
+#  master_password     = "password"
+#  port                = 27017
+#  skip_final_snapshot = true
+#
+#  vpc_security_group_ids = [
+#    aws_security_group.dbs_security_group.id,
+#    aws_security_group.schedule_security_group.id
+#  ]
+#
+#  db_subnet_group_name = aws_db_subnet_group.schedule_mongo_subnet_group.name
+#}
+#
+#resource "aws_docdb_cluster_instance" "mongo_cluster_instance" {
+#  identifier         = "schedule-docdb-instance"
+#  cluster_identifier = aws_docdb_cluster.mongo_cluster.id
+#  instance_class     = "db.t3.medium"
+#}
 # mongo configuration end
