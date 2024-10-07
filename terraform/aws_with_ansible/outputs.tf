ef7a49d3df244e1027ca40bcc7c495b3e831f290
m-o-d-e-r <moder.mode.on@gmail.com>
2024-10-07 15:52:12 +0300

    update terraform outputs

--- terraform/aws_with_ansible/outputs.tf
+++ terraform/aws_with_ansible/outputs.tf
@@ -1,12 +1,12 @@
 
 output "schedule_web" {
-  value = module.schedule_web.instance_public_ip
+  value = module.schedule_web.instance_public_dns
 }
 
 output "schedule_api" {
-  value = module.schedule_api.instance_public_ip
+  value = module.schedule_api.instance_public_dns
 }
 
 output "schedule_dbs" {
-  value = module.schedule_dbs.instance_public_ip
+  value = module.schedule_dbs.instance_public_dns
 }
